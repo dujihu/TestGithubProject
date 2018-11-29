289935f36fe8fef2dfcc9e6231c2a2b233e9c12e
dujihu <885077506@qq.com>
2018-11-29 09:35:04 +0800

    Initial commit

---
+++ src/main/java/com/aaa/testgh/controller/DeptController.java
@@ -0,0 +1,14 @@
+package com.aaa.testgh.controller;
+
+/**
+ * className:DeptController
+ * discription:
+ * author:dujihu
+ * createTime:2018-11-29 09:09
+ */
+public class DeptController {
+
+    public static void main(String[] args) {
+        System.out.println("测试：GitHub提交11111");
+    }
+}
